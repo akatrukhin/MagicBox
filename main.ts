3ea0cedca3bce9c66ba2a39ea89ddf3e6d969276
Alexander Katrukhin <olkatruk@microsoft.com>
2020-11-16 18:40:26 -0800

    Release.yml updates

--- main.ts
+++ main.ts
@@ -20,53 +20,48 @@ import {
 
 log.info("App starting...");
 
-try {
-  // This method will be called when Electron has finished
-  // initialization and is ready to create browser windows.
-  // Some APIs can only be used after this event occurs.
-  app.on("ready", () => {
-    SettingsInitialization();
-    AppMenuInit();
-    buildAppUI();
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on("ready", () => {
+  SettingsInitialization();
+  AppMenuInit();
+  buildAppUI();
 
-    if (settings.get("app.updateCheck", {}) === true) {
-      autoUpdater.checkForUpdates();
-    }
-    // Handler for when text data is copied into the clipboard
-    clipboardWatcher({
-      onTextChange: (SVGxml: string) => {
-        // XML Validator
-        if (settings.get("app.clipboardWatcher")) {
-          if (parser.validate(SVGxml) && SVGxml.includes("<svg")) {
-            optimizeClipboardSVG("get-svg-from-clipboard", SVGxml);
-          }
+  if (settings.get("app.updateCheck", {}) === true) {
+    autoUpdater.checkForUpdates();
+  }
+  // Handler for when text data is copied into the clipboard
+  clipboardWatcher({
+    onTextChange: (SVGxml: string) => {
+      // XML Validator
+      if (settings.get("app.clipboardWatcher")) {
+        if (parser.validate(SVGxml) && SVGxml.includes("<svg")) {
+          optimizeClipboardSVG("get-svg-from-clipboard", SVGxml);
         }
-      },
-    });
-
-    if (settings.get("app.updateCheck")) {
-      AutoUpdaterInit();
-    }
+      }
+    },
   });
 
-  ipcMain.on("set-auto-updater", () => AutoUpdaterInit());
-  ipcMain.on("check-for-updates", () => autoUpdater.checkForUpdates());
-  ipcMain.on("file-optimization", (event, file, customPath?: string) =>
-    ProcessFile(file, customPath)
-  );
-  ipcMain.on(
-    "show-notification",
-    (e, message: NotificationConstructorOptions) => {
-      const notification = new Notification(message);
-      notification.show();
-    }
-  );
-  ipcMain.on("quitAndInstall", (event, arg) => {
-    log.info(event);
-    log.info(arg);
-    autoUpdater.quitAndInstall();
-  });
-} catch (e) {
-  // Catch Error
-  throw e;
-}
+  if (settings.get("app.updateCheck")) {
+    AutoUpdaterInit();
+  }
+});
+
+ipcMain.on("set-auto-updater", () => AutoUpdaterInit());
+ipcMain.on("check-for-updates", () => autoUpdater.checkForUpdates());
+ipcMain.on("file-optimization", (event, file, customPath?: string) =>
+  ProcessFile(file, customPath)
+);
+ipcMain.on(
+  "show-notification",
+  (e, message: NotificationConstructorOptions) => {
+    const notification = new Notification(message);
+    notification.show();
+  }
+);
+ipcMain.on("quitAndInstall", (event, arg) => {
+  log.info(event);
+  log.info(arg);
+  autoUpdater.quitAndInstall();
+});

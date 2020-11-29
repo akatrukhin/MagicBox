8e158612df14cf040b5e6a592b0cc24c347f545b
Alexander Katrukhin <a.katrukhin@gmail.com>
2020-11-29 14:50:52 -0800

    Workflows update

--- electron/scripts/get-pkg-version.js
+++ electron/scripts/get-pkg-version.js
@@ -1,4 +1,4 @@
 // This script file simply outputs the version in the package.json.
 // It is used as a helper by the continuous integration
 https: var path = require("path");
-console.log(require(path.join(__dirname, "../package.json")).version);
+console.log(require(path.join(__dirname, "../../package.json")).version);
